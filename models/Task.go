f2e25163fb2489c97d940f71cf8969a5c6ee054c
JohnGolgota <js684new@gmail.com>
2023-12-24 08:43:20 -0500

    Faltan cosas

--- models/Task.go
+++ models/Task.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Task struct {
 	gorm.Model
 	Title       string `gorm:"not null;unique_index" json:"title"` // Add json tag
-	Description string
-	Completed   bool `gorm:"default:false" json:"completed"` // Add json tag
-	UserID      uint
+	Description string `json:"description"`                        // Add json tag
+	Completed   bool   `gorm:"default:false" json:"completed"`     // Add json tag
+	UserID      uint   `json:"user_id"`                            // Add json tag
 }
