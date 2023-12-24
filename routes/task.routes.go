72404173681575ffff1306c53dc981fc5de382fc
JohnGolgota <js684new@gmail.com>
2023-12-24 08:14:33 -0500

    task routes

--- routes/task.routes.go
+++ routes/task.routes.go
@@ -1,1 +1,61 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/JohnGolgota/fazt-go-rest-api/database"
+	"github.com/JohnGolgota/fazt-go-rest-api/models"
+	"github.com/gorilla/mux"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	database.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(&tasks)
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	json.NewDecoder(r.Body).Decode(&task)
+
+	createdTask := database.DB.Create(&task)
+	err := createdTask.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+
+	database.DB.First(&task, params["id"])
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+	json.NewEncoder(w).Encode(&task)
+}
+
+func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Update Task"))
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+
+	database.DB.First(&task, params["id"])
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+	database.DB.Delete(&task)
+	w.WriteHeader(http.StatusOK)
+}
