384e7a9a7bbc65e993eca5171a951fc893b001bb
JohnGolgota <js684new@gmail.com>
2023-12-24 00:05:52 -0500

    Sueavemente

--- models/User.go
+++ models/User.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName string `gorm:"not null"`
-	LastName  string `gorm:"not null"`
-	Email     string `gorm:"not null;unique_index"`
-	Tasks     []Task
+	FirstName string `gorm:"not null" json:"first_name"`
+	LastName  string `gorm:"not null" json:"last_name"`
+	Email     string `gorm:"not null;unique_index" json:"email"`
+	Tasks     []Task `gorm:"foreignKey:UserID" json:"tasks"`
 }
